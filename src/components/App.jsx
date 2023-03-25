da6abe6275c1e252e3546c556e731af9c88b4d50
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-03-25 18:25:51 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
+import React, { Component } from 'react';
+
+
+
 export const App = () => {
   return (
-    <div
+   <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -10,7 +14,54 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <PhoneBook />
     </div>
+  
   );
 };
+
+class PhoneBook extends Component {
+
+  state = {
+    contacts: [],
+    name: ''
+  }
+  
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    const {name} = this.state;
+    console.log(`name: ${name}`)
+    this.props.onSubmit = ({...this.state})
+    this.reset()
+  }
+
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+        <input
+        onChange={this.handleChange}
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+        </form>
+      </div>
+    )
+  }
+  
+}
+
+
+
+ 
